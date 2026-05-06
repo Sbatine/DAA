01ee24f19a25d8cb2a5cba37b607008fcdb42f8f
Shelton Rogerio Batine <sheltonbatine.sb@gmail.com>
2026-05-07 01:18:45 +0530

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+
+    int n;
+
+    cout<<"Enter number of items: ";
+    cin>>n;
+
+    int weight[n], profit[n];
+
+    cout<<"Enter weights:\n";
+    for(int i=0;i<n;i++)
+        cin>>weight[i];
+
+    cout<<"Enter profits:\n";
+    for(int i=0;i<n;i++)
+        cin>>profit[i];
+
+    int W;
+
+    cout<<"Enter capacity: ";
+    cin>>W;
+
+    int dp[n+1][W+1];
+
+    for(int i=0;i<=n;i++) {
+
+        for(int w=0;w<=W;w++) {
+
+            if(i==0 || w==0)
+                dp[i][w]=0;
+
+            else if(weight[i-1]<=w)
+                dp[i][w]=max(
+                    profit[i-1]+dp[i-1][w-weight[i-1]],
+                    dp[i-1][w]
+                );
+
+            else
+                dp[i][w]=dp[i-1][w];
+        }
+    }
+
+    cout<<"Maximum Profit = "<<dp[n][W];
+
+    return 0;
+}
